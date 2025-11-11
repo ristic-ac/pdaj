121d76f08f9e57fbd2198a95500b829d60f9c26d
ristic-branislav <branislav.ristic@42computing.tech>
2025-11-11 13:58:59 +0100

    vezbe01 update

--- 01/project_example/src/lib.rs
+++ 01/project_example/src/lib.rs
@@ -1,2 +1,4 @@
 pub mod network; // Declare the "network" module, defined in "network/mod.rs"
 pub mod utils; // Declare the "utils" module, defined in "utils/mod.rs"
+// These lines tell the Rust compiler to look for the module definitions in their respective files, and allow the main crate to access them.
+// Removing `pub` would make the modules private to the crate and inaccessible from outside, which in turn means the main binary crate would not be able to use them, i.e., it would cause compilation errors in `src/main.rs`.
--- 01/project_example/src/utils/mod.rs
+++ 01/project_example/src/utils/mod.rs
@@ -1,4 +1,7 @@
+// This file defines the "utils" module and its sub-modules, namely "helper".
+
 pub mod helper; // Declare the "helper" sub-module
+// When the compiler reads pub mod helper;, it looks for src/network/helper.rs and parses it.
 
 // Public function in "utils" module
 pub fn print_hello() {
