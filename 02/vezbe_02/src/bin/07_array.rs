701d44f0c3912438cfb49b41bddf8cd991c437ab
ristic-branislav <branislav.ristic@42computing.tech>
2025-12-03 16:10:32 +0100

    vezbe 2 refactor

--- 02/vezbe_02/src/bin/07_array.rs
+++ 02/vezbe_02/src/bin/07_array.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let a = [1, 2, 3, 4, 5];
     println!("a: {:?}", a);
@@ -8,8 +10,12 @@ fn main() {
     let second = a[1];
     println!("second: {}", second);
 
-    // let index = 10;
-    let index = 4;
+    // Input from stdin
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let index: usize = input.trim().parse().expect("Please type a number!");
+
     let element = a[index];
     println!("The value of element is: {}", element);
 }
