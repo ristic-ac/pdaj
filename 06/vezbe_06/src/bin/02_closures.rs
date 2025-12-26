0f7bf7f65078fd358f9ddf2a6d431e72e75dfa25
ristic-branislav <branislav.ristic@42computing.tech>
2025-12-26 23:09:54 +0100

    Updated example 2

--- 06/vezbe_06/src/bin/02_closures.rs
+++ 06/vezbe_06/src/bin/02_closures.rs
@@ -1,5 +1,3 @@
-use std::thread;
-
 fn immutable_reference() {
     let list = vec![1, 2, 3];
     println!("Before defining closure: {list:?}");
@@ -8,7 +6,7 @@ fn immutable_reference() {
     let only_borrows = || println!("From closure: {list:?}");
 
     println!("Before calling closure: {list:?}");
-    only_borrows();
+    only_borrows(); // This gets evaluated to Fn::call(&self, ...)
     println!("After calling closure ref: {list_ref:?}");
     println!("After calling closure: {list:?}");
 }
@@ -18,9 +16,9 @@ fn mutable_reference() {
     println!("Before defining closure: {list:?}");
     // let list_ref = &list;
 
-    let mut borrows_mutably = || list.push(7);
+    let mut borrows_mutably = || list.push(7);  // Closure mutably borrows `list`
 
-    borrows_mutably();
+    borrows_mutably(); // This gets evaluated to FnMut::call_mut(&mut self, ...)
     // println!("After calling closure once: {list_ref:?}");
     println!("After calling closure: {list:?}");
 }
@@ -29,9 +27,14 @@ fn move_ownership() {
     let list = vec![1, 2, 3];
     println!("Before defining closure: {list:?}");
 
-    thread::spawn(move || println!("From thread: {list:?}"))
-        .join()
-        .unwrap();
+    // Move ownership of `list` into the closure
+    let consumes_list = move || {
+        println!("From closure: {list:?}");
+        drop(list);
+    };
+
+    consumes_list(); // This gets evaluated to FnOnce::call_once(self, ...)
+    // consumes_list(); // This will cause a compile-time error because `list` has been moved
 }
 
 fn main(){
